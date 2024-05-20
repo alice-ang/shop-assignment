4f9ff5c8145fd31dd5390c8f7c17fca37dbe4039
Alice A <alice.anglesjo@hotmail.com>
2024-05-20 12:15:20 +0200

    fix: layzy

--- src/components/ProductItem.tsx
+++ src/components/ProductItem.tsx
@@ -27,6 +27,7 @@ export const ProductItem: FC<ProductItemProps> = ({ product }) => {
               src={`https://www.bortakvall.se/${product.images.thumbnail}`}
               alt={product.name}
               fill
+              loading="lazy"
               sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
               className="aspect-square hover:scale-110 transition-all ease-in-out"
             />
